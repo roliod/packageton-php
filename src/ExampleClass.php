53cdc9a52e27acb518ea674066745fec4c7fc3c0
roliod <rolandoduberu@gmail.com>
2019-07-20 21:48:14 +0200

    sample test

---
+++ src/ExampleClass.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Roliod\PackagetonPhp;
+
+class ExampleClass
+{
+    /**
+     * Example method.
+     *
+     * @return bool
+     */
+    public function isExample(): bool
+    {
+        return true;
+    }
+}
